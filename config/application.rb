d512dc9b941241aa2c042b90a0c7348ce8560727
Dustin Platte <dustinplatte@gmail.com>
2013-08-06 15:43:23 -0700

    Added a Ruby version to the Gemfile and stopped precompiling assets for Heroku.

--- config/application.rb
+++ config/application.rb
@@ -71,5 +71,9 @@ module DemoSpree
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
+
   end
 end
