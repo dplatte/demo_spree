4f689fc7459ec9669b2b9a2e68700569b512eec1
Dustin Platte <dustinplatte@gmail.com>
2013-08-06 15:09:15 -0700

    Demo Spree site to walk through with clients.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoSpree::Application.config.secret_token = '843b4be05de3a654aef1edbdc27475e8f7f5b821c0adba3d190cb93afcad8c6e0b5821559002727076513b317ad48e4c79aff185ae9cdc82ff7f17b59b7ff2c0'
